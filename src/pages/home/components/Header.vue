68b155aecc98747440212fa5d40fd3a8000d2cd0
BnuzLeon <769708025@qq.com>
2019-02-20 20:53:13 +0800

    Home搜索框开发

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -1,8 +1,16 @@
 <template>
   <div class="header">
-    <div class="header-left">返回</div>
-    <div class="header-input">输入城市/景点/游玩主题</div>
-    <div class="header-right">城市</div>
+    <div class="header-left">
+      <div class="iconfont back-icon">&#xe624;</div>
+    </div>
+    <div class="header-input">
+      <span class="iconfont">&#xe632;</span>
+      输入城市/景点/游玩主题
+    </div>
+    <div class="header-right">
+      城市
+      <span class="iconfont drop-icon">&#xe64a;</span>
+    </div>
   </div>
 </template>
 
@@ -13,23 +21,28 @@
 </script>
 
 <style lang="stylus" scoped>
+  @import "~@/assets/styles/variables.styl"
   /*1rem = html font-size = 50px
     例如两倍图： 86px/50*2 = 0.86rem
   */
   .header
     display flex
     line-height .86rem
-    background #00bcd4
+    background $bg-color
     color #fff
     .header-left
       width .64rem
       float left
+      .back-icon
+        text-align center
+        font-size .4rem
     .header-input
       flex 1
       background #fff
       border-radius .1rem
       margin-top .12rem
       margin-left .2rem
+      padding-left .2rem
       height .64rem
       color #ccc
       line-height .64rem
@@ -37,4 +50,7 @@
       width 1.24rem
       float right
       text-align center
+      .drop-icon
+        margin-left -.04rem
+        font-size .2rem
 </style>
