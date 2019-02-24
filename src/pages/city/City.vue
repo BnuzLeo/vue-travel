5f14521860748729697363ab74b3055a51104331
BnuzLeon <769708025@qq.com>
2019-02-24 19:52:25 +0800

    搜索功能

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <city-header></city-header>
-    <city-search></city-search>
+    <city-search :cities="cities"></city-search>
     <city-list :lists="cities"
                :hots="hotCities"
                :letter="letter"
--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -1,13 +1,59 @@
 <template>
-  <div class="search">
-    <input type="text" class="search-input" placeholder="请输入城市名称或者拼音">
+  <div>
+    <div class="search">
+      <input type="text" class="search-input" placeholder="请输入城市名称或者拼音" v-model="inputContext">
+    </div>
+    <div class="search-context" v-show="inputContext" ref="searchResults">
+      <ul class="search-context-list">
+        <li class="search-item border-bottom" v-for="(item,index) of this.results">{{item.name}}</li>
+        <li class="search-item border-bottom" v-show="hasNoData">找不到相关数据</li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
-    export default {
-        name: "Search"
+  import BSroll from 'better-scroll'
+  export default {
+    name: "Search",
+    props: {
+      cities: Object
+    },
+    data() {
+      return {
+        inputContext: '',
+        results:[],
+        timer: null
+      }
+    },
+    mounted(){
+      this.scroll = new BSroll(this.$refs.searchResults)
+    },
+    computed:{
+      hasNoData(){
+        return !this.result
+      }
+    },
+    watch: {
+      inputContext() {
+        if (this.timer) {
+          clearTimeout(this.timer)
+        }
+        const list = []
+        const key = this.inputContext;
+        this.timer = setTimeout(()=>{
+          for(let city in this.cities){
+            this.cities[city].forEach(function(item){
+              if (item.name.indexOf(key) > -1 || item.spell.indexOf(key) > -1){
+                list.push(item)
+              }
+            })
+          }
+          this.results = list;
+        },100)
+      }
     }
+  }
 </script>
 
 <style scoped lang="stylus">
@@ -26,4 +72,18 @@
       line-height .62rem
       height .62rem
 
+  .search-context
+    overflow hidden
+    position absolute
+    top 1.58rem
+    left 0
+    right 0
+    bottom 0
+    background-color #eee
+    z-index: 1
+    .search-item
+      height .86rem
+      line-height .86rem
+      padding 0 .2rem
+      background-color white
 </style>
