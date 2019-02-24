a297e31874c23ef9fbad667d4955b7e1631977fa
BnuzLeon <769708025@qq.com>
2019-02-24 13:48:17 +0800

    搜索框

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -1,13 +1,17 @@
 <template>
-  <city-header></city-header>
+  <div>
+    <city-header></city-header>
+    <city-search></city-search>
+  </div>
 </template>
 
 <script>
   import CityHeader from '@/pages/city/components/Header'
+  import CitySearch from '@/pages/city/components/Search'
 
   export default {
     name: "City",
-    components: {CityHeader}
+    components: {CityHeader,CitySearch}
   }
 </script>
 
---
+++ src/pages/city/components/Search.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="search">
+    <input type="text" class="search-input" placeholder="请输入城市名称或者拼音">
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "Search"
+    }
+</script>
+
+<style scoped lang="stylus">
+  @import "~styl/variables.styl"
+  .search
+    height .72rem
+    background-color $bg-color
+    padding 0 .1rem
+    .search-input
+      box-sizing border-box
+      width 100%
+      border-radius .06rem
+      padding 0 .1rem
+      color #666
+      text-align center
+      line-height .62rem
+      height .62rem
+
+</style>
