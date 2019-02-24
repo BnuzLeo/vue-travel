0d87adacd0fd8127ea789fb530a5c903ca04fe26
BnuzLeon <769708025@qq.com>
2019-02-24 14:26:37 +0800

    城市列表

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,16 +2,18 @@
   <div>
     <city-header></city-header>
     <city-search></city-search>
+    <city-list></city-list>
   </div>
 </template>
 
 <script>
   import CityHeader from '@/pages/city/components/Header'
   import CitySearch from '@/pages/city/components/Search'
+  import CityList from '@/pages/city/components/List'
 
   export default {
     name: "City",
-    components: {CityHeader,CitySearch}
+    components: {CityHeader,CitySearch,CityList}
   }
 </script>
 
---
+++ src/pages/city/components/List.vue
@@ -0,0 +1,115 @@
+<template>
+    <div class="list">
+      <div class="area">
+        <div class="title">当前城市</div>
+        <div class="button-list border-topbottom">
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
+        </div>
+      </div>
+      <div class="area">
+        <div class="title">热门城市</div>
+        <div class="button-list border-topbottom">
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
+        </div>
+      </div>
+      <div class="area">
+        <div class="title">A</div>
+        <div class="item-list">
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+        </div>
+      </div>
+      <div class="area">
+        <div class="title">A</div>
+        <div class="item-list">
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+        </div>
+      </div>
+      <div class="area">
+        <div class="title">A</div>
+        <div class="item-list">
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+        </div>
+      </div>
+      <div class="area">
+        <div class="title">A</div>
+        <div class="item-list">
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "List"
+    }
+</script>
+
+<style lang="stylus" scoped>
+  @import "~styl/variables.styl"
+  /*改写一像素边框的样式*/
+  .border-topbottom
+    &:before
+      background-color #ccc
+    &:after
+      background-color #ccc
+  .border-bottom
+    &:before
+      background-color #ccc
+  .list
+    overflow hidden
+    position absolute
+    top 1.58rem
+    left 0
+    right 0
+    bottom 0
+    .title
+      line-height .4rem
+      background-color #eee
+      padding-left .2rem
+      color #666
+      font-size .23rem
+    .button-list
+      padding .1rem .6rem .1rem .1rem
+      overflow hidden
+      .button-wrapper
+        float left
+        width 33.33%
+        .button
+          margin .1rem
+          padding .1rem 0
+          text-align center
+          border .02rem solid #ccc
+          border-radius .06rem
+    .item-list
+      .item
+        height .86rem
+        line-height .86rem
+        padding 0 .2rem
+</style>
